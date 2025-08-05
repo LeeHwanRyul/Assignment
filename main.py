4a081027b199a634c4776149fe117e158427f910
Djklsfj_Ryul <102909120+Djklsfj-Ryul@users.noreply.github.com>
2025-08-06 01:09:10 +0900

    FourierSeries with SawtoothWave
    plot
    N = 2, 3, 4 5
    eulerList = 5, 10, 50, 100

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+from FourierSeries import FourierSeries
+import matplotlib.pyplot as plt
+
+def SawtoothWave(t, amplitude, period):
+    return amplitude * ((t % period) / period) - (amplitude / 2)
+
+def PeriodicWave():
+    return 0
+
+if __name__ == "__main__":
+    T = 0.02
+    dx = 0.0001
+    t = np.arange(0, T * 10, dx)
+    amp = 2
+
+    x = SawtoothWave(t, amp, T)
+
+    NList = [2, 3, 4, 5]
+    eulerList = [5, 10, 50, 100]
+
+    fig, axs = plt.subplots(4, 4, figsize=(20, 12))
+    fig.suptitle("Fourier Series")
+
+    xFourier = [[0 for col in range(len(eulerList))] for row in range(len(NList))]
+
+    for j in range(len(NList)):
+        for i in range(len(eulerList)):
+            ax = axs[j][i]
+            xFourier[j][i] = FourierSeries(t, NList[j], SawtoothWave, amp, T, eulerList[i])
+
+            ax.plot(t, x, label="Original")
+            ax.plot(t, xFourier[j][i], label="Fourier", linestyle='--')
+
+            ax.set_title(f"N={NList[j]}, eulerCoef={eulerList[i]}", fontsize=10)
+            ax.set_xlim(0, T * 5) # 0 ~ 0.1
+
+    handles, labels = ax.get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=2)
+    plt.show()
